8a75b82abc25b684da0221ca7a6cfb0da5ae6a67
Carmen Juaristi <cjuaristip22@fpcoruna.afundacion.org>
2024-03-12 09:55:19 +0100

    Completada tarea #2

---
+++ app/src/main/java/com/example/pokerecuperaciont2/Celda.java
@@ -0,0 +1,13 @@
+package com.example.pokerecuperaciont2;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Celda extends RecyclerView.ViewHolder {
+
+    public Celda(@NonNull View itemView) {
+        super(itemView);
+    }
+}
